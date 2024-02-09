2ec4dbdd99bae211684cefcb0fdc0fa522876bfd
anikkarmakar3 <anikkarmakar3@gmail.com>
2024-02-10 00:17:50 +0530

    connection almost done.

--- src/app.js
+++ src/app.js
@@ -2,91 +2,159 @@ const express = require("express");
 require("../src/db/conn");
 const Post = require("../src/models/posts");
 const Profile = require("../src/models/profile");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const router = express.Router();
-const twilio = require('twilio');
+const twilio = require("twilio");
 const app = express();
-const port = process.env.port || 3000; 
+const port = process.env.port || 3000;
 
 //SDFYRNCYCTJX8YE1G7HDUSZM  This is the recovery
 // Twilio credentials
-const accountSid = 'ACe21dc0ed41d7f59f855f1d6e6b227904';
-const authToken = '5b9ac5802fbd9cbab0e25bfb06ef38c8';
-const twilioPhoneNumber = '+18644775435';
+const accountSid = "ACe21dc0ed41d7f59f855f1d6e6b227904";
+const authToken = "5b9ac5802fbd9cbab0e25bfb06ef38c8";
+const twilioPhoneNumber = "+18644775435";
 
 const client = twilio(accountSid, authToken);
 
 app.use(express.json());
 
-app.get("/getposts/pagenumeber=:pagenumeber/pagesize=:pagesize",async(req,res)=>{
-    try{
-        // Pagination parameters
-        const page = req.params.pagenumeber; // Current page number
-        const pageSize = req.params.pagesize; // Number of posts per page
+app.get(
+  "/getposts/pagenumeber=:pagenumeber/pagesize=:pagesize",
+  async (req, res) => {
+    try {
+      // Pagination parameters
+      const page = req.params.pagenumeber; // Current page number
+      const pageSize = req.params.pagesize; // Number of posts per page
 
-        // Calculate the number of documents to skip
-        const skip = (page - 1) * pageSize;
-        Post.find({}).skip(skip).limit(pageSize).then((post)=>{
-            Post.countDocuments({})
-            .then(count => {
-                // Attach the count to the response
-                res.status(200).json({totalcount:count,data:post})
-            });
-        }).catch((e)=>{
-            console.error("Error saving post:", error);
-            res.status(500).json({ error: "Internal Server Error", error });
+      // Calculate the number of documents to skip
+      const skip = (page - 1) * pageSize;
+      Post.find({})
+        .skip(skip)
+        .limit(pageSize)
+        .then((post) => {
+          Post.countDocuments({}).then((count) => {
+            // Attach the count to the response
+            res.status(200).json({ totalcount: count, data: post });
+          });
         })
-    }catch(error){
-        res.status(400).json({"message":"Can not fetch data."})
+        .catch((e) => {
+          console.error("Error saving post:", error);
+          res.status(500).json({ error: "Internal Server Error", error });
+        });
+    } catch (error) {
+      res.status(400).json({ message: "Can not fetch data." });
     }
-})
+  }
+);
 
-app.get("/getuserposts/userid=:userid",async(req, res)=>{
-    try{
-        const userid = req.params.userid
-        Post.find({"author":userid}).then((post)=>{
-            Post.countDocuments({author:userid})
-            .then(count => {
-                res.status(200).json({count:count,data:post})
-            });
-        }).catch((e)=>{
-            console.error("Error saving post:", error);
-            res.status(500).json({ error: "Internal Server Error", error });
-        })
-    }catch(e){
-        res.status(400).json({"message":"Can not fetch data."})
-    }
-})
-
-app.get("/get-profile/userid=:userid",async(req, res)=>{
-    try{
-        const userid = req.params.userid
-        console.log(userid)
-        if (userid.match(/^[0-9a-fA-F]{24}$/)) {
-            // Yes, it's a valid ObjectId, proceed with `findById` call.
-            Profile.findById(userid).then((profile)=>{
-                res.status(200).json({data:profile})
-            }).catch((error) => {
-                console.error("Error saving post:", error);
-                res.status(500).json({ error: "Internal Server Error", error });
-              });
-        }
-        else{
-            res.status(400).json({"message":"id is not valid."})
-        }
-    }catch(e){
-        res.status(400).json({"message":"Can not fetch data."})
-    }
-})
-
-app.patch("/editpost/postid=:postid",async(req,res)=>{
-  try{
-    const postid = req.params.postid;  
-    console.log(req.body);
-    Post
-      .findByIdAndUpdate(postid,req.body,{
-        new:true
+app.get("/getuserposts/userid=:userid", async (req, res) => {
+  try {
+    const userid = req.params.userid;
+    Post.find({ author: userid })
+      .then((post) => {
+        Post.countDocuments({ author: userid }).then((count) => {
+          res.status(200).json({ count: count, data: post });
+        });
       })
+      .catch((e) => {
+        console.error("Error saving post:", error);
+        res.status(500).json({ error: "Internal Server Error", error });
+      });
+  } catch (e) {
+    res.status(400).json({ message: "Can not fetch data." });
+  }
+});
+
+app.post("/requestconnections/userid=:userid", async (req, res) => {
+  try {
+    const userid = req.params.userid;
+    const connectionid = req.body.connectionid;
+    //before add connections id check connections is already exist or not
+    console.log("user id is ", userid);
+    console.log("connection id is", connectionid);
+    Profile.findOneAndUpdate(
+      { _id: userid, requestedConnections: { $ne: connectionid } }, // Check if connectionid doesn't exist
+      { $addToSet: { requestedConnections: connectionid } }, // Add connectionid if it doesn't exist
+      { new: true }
+    )
+      .then((profile) => {
+        if (profile) {
+          res
+            .status(200)
+            .json({ message: "request succesfully", data: profile });
+        } else {
+          res
+            .status(400)
+            .json({
+              message:
+                "Connection ID already exists in the requested connections array.",
+            });
+        }
+      })
+      .catch((e) => {
+        console.error("Error saving post:", e);
+        res.status(500).json({ error: "Internal Server Error", e });
+      });
+  } catch (e) {
+    res.status(400).json({ message: "Can not fetch data." });
+  }
+});
+
+app.post("/acceptconnections/userid=:userid", async (req, res) => {
+  try {
+    const userid = req.params.userid;
+    const connectionid = req.body.connectionid;
+    console.log("user id is ", userid);
+    console.log("connection id is", connectionid);
+    Profile.findOneAndUpdate(
+      { _id: userid },
+      { $push: { connections: connectionid } },
+      { $pull: { requestedConnections: connectionid } },
+      { new: true }
+    )
+      .then((profile) => {
+        res
+          .status(200)
+          .json({ message: "request accept successfuly", data: profile });
+      })
+      .catch((e) => {
+        console.error("Error saving post:", error);
+        res.status(500).json({ error: "Internal Server Error", error });
+      });
+  } catch (e) {
+    res.status(400).json({ message: "Can not fetch data." });
+  }
+});
+
+app.get("/get-profile/userid=:userid", async (req, res) => {
+  try {
+    const userid = req.params.userid;
+    console.log(userid);
+    if (userid.match(/^[0-9a-fA-F]{24}$/)) {
+      // Yes, it's a valid ObjectId, proceed with `findById` call.
+      Profile.findById(userid)
+        .then((profile) => {
+          res.status(200).json({ data: profile });
+        })
+        .catch((error) => {
+          console.error("Error saving post:", error);
+          res.status(500).json({ error: "Internal Server Error", error });
+        });
+    } else {
+      res.status(400).json({ message: "id is not valid." });
+    }
+  } catch (e) {
+    res.status(400).json({ message: "Can not fetch data." });
+  }
+});
+
+app.patch("/editpost/postid=:postid", async (req, res) => {
+  try {
+    const postid = req.params.postid;
+    console.log(req.body);
+    Post.findByIdAndUpdate(postid, req.body, {
+      new: true,
+    })
       .then((post) => {
         // Construct the response object
         const response = {
@@ -94,54 +162,51 @@ app.patch("/editpost/postid=:postid",async(req,res)=>{
         };
         // Send the response back to the client
         res.status(200).json(response);
-      })  
+      })
       .catch((error) => {
         console.error("Error saving post:", error);
         res.status(500).json({ error: "Internal Server Error", error });
       });
-    }catch(error){
-        res.send(e);
-    }  
-})
+  } catch (error) {
+    res.send(e);
+  }
+});
 
-app.delete("/deletepost/postid=:postid",async(req,res)=>{
-    try{
-    const postid = req.params.postid;  
+app.delete("/deletepost/postid=:postid", async (req, res) => {
+  try {
+    const postid = req.params.postid;
     console.log(req.body);
-    Post
-      .findByIdAndDelete(postid)
+    Post.findByIdAndDelete(postid)
       .then((post) => {
         // Construct the response object
         Profile.updateMany(
           { posts: postid },
           { $pull: { posts: postid } }
-      ).then(()=>{
-        const response = {
-          message: "Data delete successfully",
-        };
-        // Send the response back to the client
-        res.status(200).json(response);
-      })
-    
+        ).then(() => {
+          const response = {
+            message: "Data delete successfully",
+          };
+          // Send the response back to the client
+          res.status(200).json(response);
+        });
       })
       .catch((error) => {
         console.error("Error saving post:", error);
         res.status(500).json({ error: "Internal Server Error", error });
       });
-    }catch(error){
-        res.send(e);
-    }
-})
+  } catch (error) {
+    res.send(e);
+  }
+});
 
-app.patch("/updateprofile/userid=:userid", async(req,res)=>{
-    try{
+app.patch("/updateprofile/userid=:userid", async (req, res) => {
+  try {
     const userid = req.params.userid;
     // const profileRecord = new Profile(req.body);
     console.log(req.body);
-    Profile
-      .findByIdAndUpdate(userid,req.body,{
-        new:true
-      })
+    Profile.findByIdAndUpdate(userid, req.body, {
+      new: true,
+    })
       .then((profile) => {
         // Construct the response object
         const response = {
@@ -155,14 +220,14 @@ app.patch("/updateprofile/userid=:userid", async(req,res)=>{
         console.error("Error saving post:", error);
         res.status(500).json({ error: "Internal Server Error", error });
       });
-    }catch(e){
-        res.send(e);
-    }
-})
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 //Post api for store phone number on profile collections
-app.post("/profile", async(req,res)=>{
-    try{
+app.post("/profile", async (req, res) => {
+  try {
     const profileRecord = new Profile(req.body);
     console.log(req.body);
     profileRecord
@@ -180,24 +245,26 @@ app.post("/profile", async(req,res)=>{
         console.error("Error saving post:", error);
         res.status(500).json({ error: "Internal Server Error", error });
       });
-    }catch(e){
-        res.send(e);
-    }
-})
+  } catch (e) {
+    res.send(e);
+  }
+});
 //Post api for store post details
 app.post("/post", async (req, res) => {
   try {
     // const { content,author } = req.body;
     const postRecords = new Post(req.body);
-    console.log(req.body)
-    postRecords.save()
+    console.log(req.body);
+    postRecords
+      .save()
       .then((newpost) => {
-        console.log(newpost.content)
+        console.log(newpost.content);
         // Update the user's posts array with the newly created post
-        Profile.findByIdAndUpdate(newpost.author, { $push: { posts: newpost._id } })
-        .then((profile)=>{
-            console.log(profile.posts)
-        })
+        Profile.findByIdAndUpdate(newpost.author, {
+          $push: { posts: newpost._id },
+        }).then((profile) => {
+          console.log(profile.posts);
+        });
         // Construct the response object
         const response = {
           message: "Data inserted successfully",
@@ -224,39 +291,39 @@ app.listen(port, () => {
 });
 
 // Route handler to send OTP
-app.post('/send-otp', (req, res) => {
-    const { phoneNumber } = req.body;
+app.post("/send-otp", (req, res) => {
+  const { phoneNumber } = req.body;
 
-    // Generate a random 6-digit OTP
-    const otp = Math.floor(100000 + Math.random() * 900000);
+  // Generate a random 6-digit OTP
+  const otp = Math.floor(100000 + Math.random() * 900000);
 
-    // Send OTP via SMS using Twilio
-    client.messages
-        .create({
-            body: `Your OTP is: ${otp}`,
-            from: twilioPhoneNumber,
-            to: phoneNumber
-        })
-        .then(message => {
-            console.log(`OTP sent: ${message.sid}`);
-            res.json({ success: true, message: 'OTP sent successfully' });
-        })
-        .catch(error => {
-            console.error('Error sending OTP:', error);
-            res.status(500).json({ success: false, message: 'Failed to send OTP' });
-        });
+  // Send OTP via SMS using Twilio
+  client.messages
+    .create({
+      body: `Your OTP is: ${otp}`,
+      from: twilioPhoneNumber,
+      to: phoneNumber,
+    })
+    .then((message) => {
+      console.log(`OTP sent: ${message.sid}`);
+      res.json({ success: true, message: "OTP sent successfully" });
+    })
+    .catch((error) => {
+      console.error("Error sending OTP:", error);
+      res.status(500).json({ success: false, message: "Failed to send OTP" });
+    });
 });
 
 // Route handler to validate OTP
-app.post('/validate-otp', (req, res) => {
-    const { otp, userEnteredOTP } = req.body;
+app.post("/validate-otp", (req, res) => {
+  const { otp, userEnteredOTP } = req.body;
 
-    // Validate OTP
-    if (otp === userEnteredOTP) {
-        res.status(200).json({ success: true, message: 'OTP validated successfully' });
-    } else {
-        res.status(400).json({ success: false, message: 'Invalid OTP' });
-    }
+  // Validate OTP
+  if (otp === userEnteredOTP) {
+    res
+      .status(200)
+      .json({ success: true, message: "OTP validated successfully" });
+  } else {
+    res.status(400).json({ success: false, message: "Invalid OTP" });
+  }
 });
-
-
