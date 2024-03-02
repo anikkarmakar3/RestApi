5be5848b503e94b2dc477e919816f3e1af1c87e5
Anik Karmakar <94543398+anikkarmakar3@users.noreply.github.com>
2024-03-02 22:22:52 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/posts",{
+mongoose.connect("mongodb+srv://anikkarmakar3:YJjhW6YQJ29u31us@cluster0.wovbies.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
 }).then(()=>{
     console.log("db connecting successfully")
 }).catch((e)=>{
